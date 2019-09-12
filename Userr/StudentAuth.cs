f408b3a48f9cddf9dd55d468990b6d1c530e64a1
Оспанов Диас <dasik.osp@mail.ru>
2019-09-12 13:39:32 +0600

    Добавьте файлы проекта.

---
+++ Userr/StudentAuth.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Userr
+{
+    public class StudentAuth : IAuthorize
+    {
+        public bool Authorize(string login, string password)
+        {
+            string cmdStudents = "SELECT * FROM Users WHERE id_roles = 2 AND login = " + login + " AND password = " + password + ";";
+            using (SqlConnection conn = new SqlConnection(ConnectionString.Connstr))
+            {
+                SqlCommand cmd = new SqlCommand(cmdStudents, conn);
+                var read = cmd.ExecuteNonQuery();
+                if (read != 0)
+                {
+                    return true;
+                }
+                else
+                    return false;
+            }
+        }
+    }
+}
